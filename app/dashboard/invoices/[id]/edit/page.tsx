989f188259495a833b99b1d300ce6ad8cecd47bf
Garvit <garvit@wednesday.is>
2023-12-13 18:48:22 +0530

    Add metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,6 +3,15 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 
+export function generateMetadata({ params }: { params: { id: string } }) {
+  const id = params.id;
+
+  return {
+    title: `Edit Invoice`,
+    description: `Edit Invoice ${id}`,
+  };
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
 
